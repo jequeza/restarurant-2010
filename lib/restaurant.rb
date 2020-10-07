e225d381afd9d834d73cd2635f5eab0626a98ffd
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-07 14:31:23 -0500

    Add name variable removing the number of arguments error.

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,7 +1,9 @@
 class Restaurant
   attr_reader :opening_time
+  attr_reader :name
 
-  def initialize(opening_time)
+  def initialize(opening_time, name)
     @opening_time = opening_time
+    @name = name
   end
 end
